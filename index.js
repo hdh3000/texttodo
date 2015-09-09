69c93939e588d73e39b55daef3890127a0d4c865
Hack Reactor Students <accounts@hackreactor.com>
2015-09-08 21:42:21 -0700

    basic skeleton of routing, and wrote data to text functionality

--- index.js
+++ index.js
@@ -8,13 +8,26 @@ var app = express()
 var port = process.env.PORT || 5000;
 var ip = process.env.IP || "127.0.0.1";
 
+//adds a new to do
+app.post('/newtodo', function(response, request){
+  
+});
 
+//returns a to do
+app.get('/tobedone', function(response, request){
+
+});
+
+//removes a done to-do
+app.post('/tobedone', function(response, request){
+
+});
 
 
 //basic server setup for http
 var server = app.listen(port, function () {
   var host = ip;
   var port = server.address().port;
-  
+
   console.log('Example app listening at http://%s:%s', host, port);
 });
