a15bdcc45261dabae06f8c1a30ce55e2f0862ef4
Hack Reactor Students <accounts@hackreactor.com>
2015-09-09 22:22:40 -0700

    added comments of post output

--- data.js
+++ data.js
@@ -10,29 +10,29 @@ var initialize = function() {
     var results = JSON.parse(fs.readFileSync(entriesPath));
     entryID = results.length + 1;
     entries = results;
-}
+};
 
 
 var addEntry = function(entry){
   //add metadata to new entries
-  entry['createdAt'] = new Date();
-  entry['entryID'] = entryID;
+  entry.createdAt = new Date();
+  entry.entryID = entryID;
   entries.push(entryID);
   entryID++;
 
   //save to text file
-  fs.writeFileSync(entriesPath, JSON.stringify(entries),'utf8')
-}
+  fs.writeFileSync(entriesPath, JSON.stringify(entries),'utf8');
+};
 
 //returns last entry in entries
 var returnEntry = function(){
   return entries[entries.length];
-}
+};
 
 //remove the last entry in the stack and return it
 var removeEntry = function(){
   return entries.pop();
-}
+};
 
 
 //export functionality
--- index.js
+++ index.js
@@ -42,8 +42,6 @@ app.post('/newtodo', function(req, res){
 });
 
 
-
-
 //returns a to do
 app.get('/tobedone', function(request, response){
   client.sendMessage({
@@ -77,3 +75,44 @@ var server = app.listen(port, function () {
 
   console.log('Example app listening at http://%s:%s', host, port);
 });
+
+
+// 2015-09-10T05:16:40.055691+00:00 app[web.1]:   ToState: 'MN',
+// 2015-09-10T05:16:40.055688+00:00 app[web.1]: { ToCountry: 'US',
+// 2015-09-10T05:16:40.055692+00:00 app[web.1]:   SmsMessageSid: 'SM5a7a6b279cba1542558ed3e0dcd67365',
+// 2015-09-10T05:16:40.055695+00:00 app[web.1]:   ToCity: 'MINNEAPOLIS',
+// 2015-09-10T05:16:40.055694+00:00 app[web.1]:   NumMedia: '0',
+// 2015-09-10T05:16:40.055697+00:00 app[web.1]:   FromZip: '55343',
+// 2015-09-10T05:16:40.055698+00:00 app[web.1]:   SmsSid: 'SM5a7a6b279cba1542558ed3e0dcd67365',
+// 2015-09-10T05:16:40.055700+00:00 app[web.1]:   FromState: 'MN',
+// 2015-09-10T05:16:40.055701+00:00 app[web.1]:   SmsStatus: 'received',
+// 2015-09-10T05:16:40.055703+00:00 app[web.1]:   FromCity: 'MINNEAPOLIS',
+// 2015-09-10T05:16:40.055704+00:00 app[web.1]:   Body: 'Yooo',
+// 2015-09-10T05:16:40.055705+00:00 app[web.1]:   FromCountry: 'US',
+// 2015-09-10T05:16:40.055707+00:00 app[web.1]:   To: '+17639511825',
+// 2015-09-10T05:16:40.055708+00:00 app[web.1]:   ToZip: '55401',
+// 2015-09-10T05:16:40.055709+00:00 app[web.1]:   NumSegments: '1',
+// 2015-09-10T05:16:40.055711+00:00 app[web.1]:   MessageSid: 'SM5a7a6b279cba1542558ed3e0dcd67365',
+// 2015-09-10T05:16:40.055712+00:00 app[web.1]:   AccountSid: 'AC06255c7484e75adfd67e8f16c75e10b7',
+// 2015-09-10T05:16:40.055713+00:00 app[web.1]:   From: '+16129108918',
+// 2015-09-10T05:16:40.055714+00:00 app[web.1]:   ApiVersion: '2010-04-01' }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
